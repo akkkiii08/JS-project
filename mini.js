bff6d8569bb3498176cb72d66815b9f231314687
Akshay Gupta <akshaygupta4918@gmail.com>
2024-06-21 13:16:18 +0530

    forth commit

---
+++ mini.js
@@ -0,0 +1,82 @@
+// Simon says game
+let gameSeq=[];
+let userSeq=[];
+let started=false;
+let level=0;
+let highestScore=0;
+let btns=["yellow","red","purple","green"];
+let h2=document.querySelector("h2");
+document.addEventListener("keypress",function(){
+    if(started==false){
+        console.log("game is started");
+        started=true;
+        levelUp();
+    }
+});
+function btnFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    },200);
+}
+function levelUp(){
+    userSeq=[];
+    level++;
+    h2.innerText=`Level ${level}`;
+    let ranIdx=Math.floor(Math.random()*3);
+    let randColor=btns[ranIdx];
+    let randBtn=document.querySelector(`.${randColor}`);
+    // console.log(ranIdx);
+    // console.log(randColor);
+    // console.log(randBtn);
+    gameSeq.push(randColor);
+    console.log(gameSeq);
+    console.log(userSeq);
+    btnFlash(randBtn);
+}
+function checkAns(idx){
+    if(userSeq[idx]===gameSeq[idx]){
+        if(userSeq.length==gameSeq.length){
+            setTimeout(levelUp,1000);
+        }
+        console.log("same value");
+        
+    }else{
+        h2.innerHTML=`Game Over!Your score was <b>${level}</b> <br> Press any key to start..`;
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(function () {
+          document.querySelector("body").style.backgroundColor = "white";
+        }, 150);
+        highScore();
+       
+        reset();
+    }
+}
+function btnPress(){
+    let btn=this;
+    btnFlash(btn);
+
+    let userColor=btn.getAttribute("id");
+    userSeq.push(userColor);
+
+    checkAns(userSeq.length-1);
+}
+let allBtns=document.querySelectorAll(".btn");
+for(btn of allBtns){
+    btn.addEventListener("click",btnPress);
+}
+function reset(){
+    started=false;
+    gameSeq=[];
+    userSeq=[];
+    level=0;
+}
+function highScore(){
+    let currentScore=level;
+    let h3=document.querySelector("h3");
+            
+        if(currentScore>highestScore){           
+            highestScore = currentScore;
+            h3.innerText = `Highest Score: ${highestScore}`;     
+        }
+}
